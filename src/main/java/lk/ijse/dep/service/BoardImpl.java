daee50459eb3527b72256705a3c65ba271e3f71a
Sameera Madushan Silva <madushansameera499@gmail.com>
2023-10-11 10:41:23 +0530

    new

--- src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -69,24 +69,56 @@ public class BoardImpl implements Board{
     @Override
     public Winner findWinner() {
 
-        //connect 4 piece horizontally
-        for (int i=0; i<NUM_OF_ROWS; i++){
-            for (int j=0; j<NUM_OF_COLS/2; j++){
-                if((!pieces[j][i].equals(Piece.EMPTY)) && pieces[j][i].equals(pieces[j+1][i]) && pieces[j][i].equals(pieces[j+2][i]) && pieces[j][i].equals(pieces[j+3][i])){
-                    return new Winner(pieces[j][i],j,i,j+3,i);
+        //HumanPlayerWinner (colWinner check)
+        for (int i = 0; i < pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length - 3; j++) {
+                if (pieces[i][j] == Piece.BLUE & pieces[i][j + 1] == Piece.BLUE & pieces[i][j + 2] == Piece.BLUE & pieces[i][j + 3] == Piece.BLUE) {
+                    int col1 = i;
+                    int row1 = j;
+                    int col2 = i;
+                    int row2 = (j + 3);
+                    return new Winner(Piece.BLUE, col1, row1, col2, row2);
                 }
             }
         }
-
-        //connect 4 pieces vertically
-        for (int j=0; j<NUM_OF_COLS; j++){
-            for (int i=0; i<NUM_OF_ROWS/2; i++){
-                if (!pieces[j][i].equals(Piece.EMPTY) && pieces[j][i].equals(pieces[j][i+1]) && pieces[j][i].equals(pieces[j][i+2]) && pieces[j][i].equals(pieces[j][i+3])){
-                    return new Winner(pieces[j][i],j,i,j,i+3);
+        //HumanPlayerWinner (rowWinner check)
+        for (int i = 0; i < pieces.length - 3; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
+                if (pieces[i][j] == Piece.BLUE & pieces[i + 1][j] == Piece.BLUE & pieces[i + 2][j] == Piece.BLUE & pieces[i + 3][j] == Piece.BLUE) {
+                    int col1 = i;
+                    int row1 = j;
+                    int col2 = (i + 3);
+                    int row2 = j;
+                    return new Winner(Piece.BLUE, col1, row1, col2, row2);
+                }
+            }
+        }
+        //Ai_PlayerWinner (colWinner check)
+        for (int i = 0; i < pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length - 3; j++) {
+                if (pieces[i][j] == Piece.GREEN & pieces[i][j + 1] == Piece.GREEN & pieces[i][j + 2] == Piece.GREEN & pieces[i][j + 3] == Piece.GREEN) {
+                    int col1 = i;
+                    int row1 = j;
+                    int col2 = i;
+                    int row2 = (j + 3);
+                    return new Winner(Piece.GREEN, col1, row1, col2, row2);
+                }
+            }
+        }
+        //Ai_PlayerWinner (rowWinner check)
+        for (int i = 0; i < pieces.length - 3; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
+                if (pieces[i][j] == Piece.GREEN & pieces[i + 1][j] == Piece.GREEN & pieces[i + 2][j] == Piece.GREEN & pieces[i + 3][j] == Piece.GREEN) {
+                    int col1 = i;
+                    int row1 = j;
+                    int col2 = (i+3);
+                    int row2 = j;
+                    return new Winner(Piece.GREEN, col1, row1, col2, row2);
                 }
             }
         }
         return new Winner(Piece.EMPTY);
+
     }
 
 }
